97c16fe68e5e5ea121fdc7660eb30cb8cd52fe35
Sage-Soulweaver <shining_kat@proton.me>
2025-02-26 14:13:07 +1300

    Update multi.lua

--- multi.lua
+++ multi.lua
@@ -22,7 +22,7 @@ RootRotation = 0
 
 RunService.Heartbeat:Connect(function()
 	LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.zero
-	
+
 	if RootTarget then do
 			LocalPlayer.Character.HumanoidRootPart.CFrame = (CFrame.new(RootTarget.Character.HumanoidRootPart.position) + RootOffset) * CFrame.Angles(0,math.rad(RootRotation),0)
 		end
@@ -31,78 +31,81 @@ RunService.Heartbeat:Connect(function()
 	end
 end)
 
-SEND = ""
 
-TextChatService.OnIncomingMessage = function(Message)
-	if Message.Text == SEND then do
-			SEND = ""
+function Chatted(Message)
+	if string.match(Message, Syntax) then
+		local Table = string.split(Message:gsub(Syntax,""),",")
+		local Target = Table[1]
+		local Timer = tonumber(Table[2])
+
+		RootTarget = Players[Target]
+		RootOffset = Vector3.new(0,-20,0)
+
+		while true do
+			if tonumber(os.date("%S")) == Timer or tonumber(os.date("%S")) == Timer + 1 then
+				--print("TIME")
+				break
+			end
+			--print(Timer,os.date("%S"))
+			RunService.Stepped:Wait()
 		end
-	else
-		if string.match(Message.Text, Syntax) then
-			local Table = string.split(Message.Text:gsub(Syntax,""),",")
-			local Target = Table[1]
-			local Timer = tonumber(Table[2])
-			
-			RootTarget = Players[Target]
-			RootOffset = Vector3.new(0,-20,0)
-			
-			while true do
-				if tonumber(os.date("%S")) == Timer or tonumber(os.date("%S")) == Timer + 1 then
-					--print("TIME")
-					break
-				end
-				--print(Timer,os.date("%S"))
-				RunService.Stepped:Wait()
-			end
-			
-			if LocalPlayer.Name == _1 then
-				RootOffset = Vector3.new(5,0,0)
-				RootRotation = 90
-			end
-			
-			if LocalPlayer.Name == _2 then
-				RootOffset = Vector3.new(-5,0,0)
-				RootRotation = -90
-			end
-			
-			TextChannel:SendAsync("YOU ARE BEING WATCHED")
-			
-			task.wait(3)
-			
-			RootOffset = Vector3.new(0,-20,0)
-			
-			task.wait(3)
-			
-			RootTarget = nil
-			RootOffset = Vector3.zero
-			
+
+		if LocalPlayer.Name == _1 then
+			RootOffset = Vector3.new(5,0,0)
+			RootRotation = 90
 		end
+
+		if LocalPlayer.Name == _2 then
+			RootOffset = Vector3.new(-5,0,0)
+			RootRotation = -90
+		end
+
+		TextChannel:SendAsync("YOU ARE BEING WATCHED")
+
+		task.wait(3)
+
+		RootOffset = Vector3.new(0,-20,0)
+
+		task.wait(3)
+
+		RootTarget = nil
+		RootOffset = Vector3.zero
+
 	end
 end
 
+wait(10)
+
 if LocalPlayer.Name == _1 then
 	SG = Instance.new("ScreenGui",LocalPlayer.PlayerGui)
-	
+
 	Back = Instance.new("Frame",SG)
 	Back.Draggable = true
 	Back.Size = UDim2.new(.2,0,.2,0)
 	Back.Position = UDim2.new(0,0,1,0)
 	Back.AnchorPoint = Vector2.new(0,1)
-	
+
 	TextBox = Instance.new("TextBox",Back)
 	TextBox.Size = UDim2.new(1,0,.5,0)
 	TextBox.TextScaled = true
-	
+
 	Button = Instance.new("TextButton",Back)
 	Button.Size = UDim2.new(1,0,.5,0)
 	Button.Position = UDim2.new(0,0,.5,0)
 	Button.Text = "Do the thing!!!"
 	Button.TextScaled = true
-	
+
 	Button.MouseButton1Up:Connect(function()
 		Timer = os.date("%S") + 20
-		if Timer > 59 then Timer -= 60 end 
-		SEND = Syntax..TextBox.Text..","..tostring(Timer)
-		TextChannel:SendAsync(SEND)
+		if Timer > 59 then Timer -= 60 end
+		local NM = Syntax..TextBox.Text..","..tostring(Timer)
+		TextChannel:SendAsync(NM)
+		Chatted(NM)
+	end)
+end
+
+for i,v in pairs(game:GetService("Players"):GetPlayers()) do
+	v.Chatted:connect(function(chat)
+		Chatted(chat)
 	end)
 end
